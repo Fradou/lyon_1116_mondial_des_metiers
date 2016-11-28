4ff23239bc4bbfa01f36f78f9c58ac8b4c888d7d
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-28 23:09:51 +0100

    Use materialize chips to do tags. Include materialize icon in cdn. Adapat autocomp script and remove useless css

--- web/js/autocomp.js
+++ web/js/autocomp.js
@@ -1,25 +1,26 @@
-/**
- * Created by alex on 28/11/16.
- */
 $(document).ready( function() {
    /** $('#tags').on('autocompletechange change', function () {
         $('#tagsname').html('You selected: ' + this.value);
     }).change(); **/
+
    var nbrep = 0;
 
    $('#tags').change(function () {
        $('#tagsname').html('You selected: ' + this.value);
        nbrep ++;
-       $('#nbreponse').html('Il vous reste encore ' + (5- nbrep) + ' reponses à donner');
-       $('#tagdisplay').after('<div class="answerchosen">X  ' + $('#tags').val() + '</div>');
+       $('#nbreponse').html('Il vous reste encore maximum ' + (5- nbrep) + ' reponses à donner.');
+       $('#tagdisplay').after(
+           '<div class="chip">' + $('#tags').val() + '<i class="close material-icons">close</i></div>');
 
+           //* '<div class="answerchosen">X  ' + $('#tags').val() + '</div>'); **/
+        console.log(  $('.chip').material_chip('data'));
    });
 
 
-   $(document).on('click', '.answerchosen', function() {
+   $(document).on('click', '.chip', function() {
        nbrep--;
        $(this).remove();
-       $('#nbreponse').html('Il vous reste encore ' + (5- nbrep) + ' reponses à donner');
+       $('#nbreponse').html('Il vous reste encore maximum ' + (5- nbrep) + ' reponses à donner.');
    });
 });
 
