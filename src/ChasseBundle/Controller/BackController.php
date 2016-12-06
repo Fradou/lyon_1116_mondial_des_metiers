8125a0bdd660eb8a39cde3b0f11eed23a16c8a1a
CilooW <cecile.angays@yahoo.fr>
2016-12-06 19:14:57 +0100

    added new stats to count registered users, active users, how many job answered, top 20 for most answered jobs and most answered domains

--- src/ChasseBundle/Controller/BackController.php
+++ src/ChasseBundle/Controller/BackController.php
@@ -8,13 +8,27 @@ class BackController extends Controller
 {
     public function statsAction()
     {
-        $userManager = $container->get('fos_user.user_manager');
+        //nb de user enregistrés
+        $userManager = $this->container->get('fos_user.user_manager');
         $users = count($userManager->findUsers());
 
+        //nb de user ayant au moins répondu à un métier
+        $activeUsers = $this->getDoctrine()->getRepository('ChasseBundle:Interview')->getCountUsers();
+
+        //nombre de métiers répondus
+        $nbjobs = $this->getDoctrine()->getRepository('ChasseBundle:Interview')->getCountJobs();
+
+        $mostAnsweredJobs = $this->getDoctrine()->getRepository('ChasseBundle:Interview')->get20jobs();
+
+        $mostAnsweredDomains = $this->getDoctrine()->getRepository('ChasseBundle:Interview')->get20domains();
 
 
         return $this->render('Back/stats.html.twig', array(
-            "totalusers" => $users
+            "totalusers" => $users,
+            "activeusers" => $activeUsers,
+            "nbjobs"    => $nbjobs,
+            "mostAnsweredJobs" => $mostAnsweredJobs,
+            "mostAnsweredDomains" => $mostAnsweredDomains,
 
         ));
     }
--- src/ChasseBundle/Repository/InterviewRepository.php
+++ src/ChasseBundle/Repository/InterviewRepository.php
@@ -3,6 +3,7 @@
 namespace ChasseBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use ChasseBundle\Entity\Job;
 
 /**
  * InterviewRepository
@@ -12,4 +13,51 @@ use Doctrine\ORM\EntityRepository;
  */
 class InterviewRepository extends EntityRepository
 {
+    public function getCountUsers() { //fonction qui compte le nombre d'entrée distinctes dans la table pour user (combien de user ont au moins répondus à un job)
+         $qb = $this->createQueryBuilder('i')
+            ->select('count(DISTINCT i.user)')
+            ->getQuery();
+
+             return $qb->getSingleScalarResult();
+    }
+
+    public function getCountJobs() { //fonction qui compte le nombre d'entrée distinctes pour 'name' dans la table (combien de jobs ont été répondus)
+        $qb = $this->createQueryBuilder('i')
+            ->select('count(DISTINCT i.job)')
+            ->getQuery();
+
+        return $qb->getSingleScalarResult();
+    }
+
+    public function get20jobs() { // fonction qui retourne les 20 métiers les plus répondus (donc c'est un array)
+        $qb = $this->createQueryBuilder('i')
+            ->select('i', 'j.name as name', 'count(i.id) as total')
+            //->innerJoin( 'i', 'Job', 'j', 'j.id = i.job')
+            ->innerJoin( 'i.job', 'j')
+            ->groupBy('i.job')
+            ->orderBy('total', 'DESC')
+            ->setMaxResults(20)
+            ->getQuery();
+
+        return $qb->getResult();
+
+    }
+
+    public function get20domains() { // fonction qui retourne les 20 domaines les plus répondus (donc c'est un array)
+        $qb = $this->createQueryBuilder('i')
+            ->select('i', 'j.domain as domain', 'count(i.id) as total')
+            ->innerJoin( 'i.job', 'j')
+            ->groupBy('j.domain')
+            ->orderBy('total', 'DESC')
+            ->setMaxResults(20)
+            ->getQuery();
+
+        return $qb->getResult();
+
+    }
+
 }
+
+/*
+
+ */
