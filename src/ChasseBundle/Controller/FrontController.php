12ad689a4ffb90e4372936322a0c1c8c8a79497a
CilooW <cecile.angays@yahoo.fr>
2016-12-16 10:31:34 +0100

    changes on css and views

--- src/ChasseBundle/Controller/FrontController.php
+++ src/ChasseBundle/Controller/FrontController.php
@@ -13,7 +13,7 @@ class FrontController extends Controller
 
     public function __construct()
     {
-        $this->openDate = new DateTime('2016-12-4 18:00:00');
+        $this->openDate = new DateTime('2017-02-2 10:00:00');
         $this->currentDate = new DateTime();
     }
 
