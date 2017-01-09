cf31250f87a5ba47725395272e8c787df689dde4
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-01-09 23:04:07 +0100

    Script working, still need some testing but look good. Refactor it to make it cleaner

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -3,17 +3,31 @@ $( document ).ready(function() {
     var nbrep = 0;
 
     function disabchoice () {
-        $('#inputword').prop('disabled', true);
+        $('#inputword').val("").prop('disabled', true);
         $("#noidea").prop('disabled', true);
         $('#wordautocomp').empty();
         console.log("disab use");
- //       document.getElementById("tags").value = "5 mots choisis, vous êtes au maximum !"
+        //       document.getElementById("tags").value = "5 mots choisis, vous êtes au maximum !"
     }
+
     function enabchoice () {
         $('#inputword').prop('disabled', false);
         $("#noidea").prop('disabled', false);
         console.log("enab use");
 
+    }
+
+    function putchips (word) {
+        html = "";
+        for (i = 0; i < word.length; i++) {
+            if ($('#chassebundle_interview_answers_' + word[i].id).is(':checked')){
+            }
+            else {
+                html +=
+                    '<div class="chip tochoose" id="'+ word[i].id +'"> # ' + word[i].word + '</div>'
+            }
+        }
+        $('#wordautocomp').html(html);
     }
 
     $("#inputword").keyup(function(){
@@ -25,50 +39,12 @@ $( document ).ready(function() {
                 dataType: 'json',
                 timeout: 3000,
                 success: function(response){
-                    var words = JSON.parse(response.data);
-                    html = "";
-                    for (i = 0; i < words.length; i++) {
-                        html +=
-                            '<div class="chip tochoose" id="'+ words[i].id +'"> # ' + words[i].word + '</div>'
-                    }
-                    $('#wordautocomp').html(html);
-                    $('.chip').on('click', function() {
-                        if ($(this).hasClass("tochoose")) {
-                            nbrep++;
-                            $(this).addClass('chosen').removeClass('tochoose').append('<i class="close material-icons">close</i>').appendTo($("#chipchosen"));
-                            console.log("j'inscremente");
-                            if (nbrep == 5) {
-                                disabchoice();
-                                $('#nbresponse').html('Vous avez atteint le maximum de réponses autorisées.');
-                            }
-                            else {
-                                $('#nbresponse').html('Il vous reste encore maximum ' + (5 - nbrep) + ' reponses à donner.');
-                            }
-                        }
-                            //    $('#ajaxbundle_contact_town').val($(this).text());
-                            //    $('#wordautocomp').html('');
-                        else {
-                            nbrep--;
-                            console.log('je delete v0');
-                            if (nbrep == 4) {
-                                enabchoice ()
-                            }
-                            $(this).remove();
-                            $('#nbresponse').html('Il vous reste encore jusqu\'à ' + (5-nbrep) + ' reponses à donner.');
-
-                        }
-                    });
-
-
-                        //      var elemid = $(this).attr('id');
-                        //     $(elemid).prop("checked", false);
-                },
+                    var result = JSON.parse(response.data);
+                    putchips(result)},
                 error: function() {
                     $('#wordautocomp').text('Ajax call error');
                 }
             });
-        } else {
-            $('#wordautocomp').html('');
         }
     });
 
@@ -79,29 +55,8 @@ $( document ).ready(function() {
             dataType: 'json',
             timeout: 3000,
             success: function(response){
-                var wordh = JSON.parse(response.data);
-                html = "";
-                for (i = 0; i < wordh.length; i++) {
-                    html +=
-                        '<div class="chip tochoose" id="'+ wordh[i].id +'"> # ' + wordh[i].word + '<i class="close material-icons">close</i></div>'
-                }
-                $('#wordautocomp').html(html);
-                $('#wordautocomp .tochoose').on('click', function() {
-                    nbrep ++;
-                    $(this).addClass('chosen').removeClass('tochoose').appendTo($("#chipchosen"));
-
-                    if (nbrep == 5) {
-                        disabchoice();
-                        $('#nbresponse').html('Vous avez atteint le maximum de réponses autorisées.');
-                    }
-                    else {
-                        $('#nbresponse').html('Il vous reste encore maximum ' + (5 - nbrep) + ' reponses à donner.');
-                    }
-
-               //     $('#ajaxbundle_contact_town').val($(this).text());
-               //     $('#wordautocomp').html('');
-                });
-            },
+                var result = JSON.parse(response.data);
+                putchips(result)},
             error: function() {
                 $('#wordautocomp').text('Ajax call error');
             }
@@ -109,4 +64,36 @@ $( document ).ready(function() {
 
     });
 
+    $(document).on('click', '.chip', function() {
+        var chipclicked = '#chassebundle_interview_answers_' + $(this).attr('id');
+        if ($(this).hasClass("tochoose")) {
+            if ($(chipclicked).is(':checked')) {
+                $('#nbresponse').html('Mot déjà selectionné ! mais il reste '+ (5 - nbrep))
+            }
+            else
+            {
+                nbrep++;
+                $(chipclicked).prop( "checked", true );
+                $(this).addClass('chosen').removeClass('tochoose').append('<i class="close material-icons">close</i>').appendTo($("#chipchosen"));
+                console.log("j'incremente");
+                if (nbrep == 5) {
+                    disabchoice();
+                    $('#nbresponse').html('Vous avez atteint le maximum de réponses autorisées.');
+                }
+                else {
+                    $('#nbresponse').html('Il vous reste encore maximum ' + (5 - nbrep) + ' reponses à donner.');
+                }
+            }
+        }
+        else {
+            nbrep--;
+            console.log('je delete v0');
+            $(chipclicked).prop("checked", false);
+            $(this).remove();
+            if (nbrep == 4) {
+                enabchoice();
+            }
+            $('#nbresponse').html('Il vous reste encore jusqu\'à ' + (5-nbrep) + ' reponses à donner.');
+        }
+    });
 });
